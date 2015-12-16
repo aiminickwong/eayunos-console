7e6ce018f42e1be1856cab1c3053da3fb08d8052
zhangyaqi <zhangyaqi@localhost.localdomain>
2015-12-16 23:08:19 +0800

    update productInfo.py

--- productInfo.py
+++ productInfo.py
@@ -27,20 +27,29 @@ button_product = urwid.Button(u'产品信息')
 button_exit = urwid.Button(u'返回')
 button_next = urwid.Button(u'下一步')
 
-product_list = [txt, button_product]
+product_list = [button_product, txtInfo]
 button_list = [button_exit, button_next]
 
+# 架构信息
 product_div = urwid.GridFlow([urwid.AttrWrap(txt, 'streak')], 38, 5, 0, 'center')
+# 产品信息按钮
 button_product_div = urwid.GridFlow([urwid.AttrWrap(button_product, 'btn')], 15, 5, 0, 'center')
+# 显示信息
 product_info_div = urwid.GridFlow([urwid.AttrWrap(txtInfo, 'streak')], 38, 5, 0, 'center')
+# 返回按钮 & 下一步按钮
 button_div = urwid.GridFlow([urwid.AttrWrap(button, 'btn', 'btn') for button in button_list], 10, 5, 0, 'center')
 div = urwid.Divider()
 
 pile = urwid.Pile([product_div, div, button_product_div, div, div, button_div])
 
+def change_pile():
+    pile = urwid.Pile([product_div, div, product_info_div, div, button_div])
+    print pile
+
 def on_product_clicked(button):
 #    pile = urwid.Pile([product_div, div, product_info_div, div, button_div])
-#    pile = urwid.Pile([])
+#    product_div = urwid.GridFlow([urwid.AttrWrap(product_thing, 'btn', 'streak') for product_thing in product_list], 15, 5, 0, 'center')
+    change_pile()
 
 def on_next_clicked(button):
     checkBox.run_checkbox()    
