7d9fe39444ab0410f6075408b26c4e73d10c02e2
walteryang <walteryang47@users.noreply.github.com>
2016-04-15 01:47:34 +0800

    Add save button and its callback for each availabble nic
    Signed-off-by: walteryang <walteryang47@users.noreply.github.com>

--- manager_console/tabnetwork.py
+++ manager_console/tabnetwork.py
@@ -19,28 +19,30 @@ class TabNetwork(object):
 
         self.widget_list.extend(self.get_ifs_widgets())
 
-        self.widget_list.extend([
-            urwid.Divider("-"),
-            urwid.Divider(" "),
-        ])
-        save = urwid.Button(u"Save")
-        urwid.connect_signal(save, 'click', self.on_save)
-        self.widget_list.append(save)
-
         self.widget = urwid.Pile(self.widget_list)
 
     def get_ifs_widgets(self):
         ifs_info_widget = []
         for interface in ifconfig.iterifs(physical=True):
+            address_edit = urwid.Edit(u"Adress: ", str(interface.get_ip()))
+            netmask_edit = urwid.Edit(u"Netmask: ", str(interface.get_netmask()))
             ifs_info_widget.append(urwid.Pile([
                 urwid.Divider("-"),
                 urwid.Text(u"Name: " + interface.name),
-                urwid.Edit(u"Adress: ", str(interface.get_ip())),
-                urwid.Edit(u"Netmask: ", str(interface.get_netmask())),
-                urwid.Text(u"Status: " + ("up" if interface.is_up() else "down"))
+                address_edit,
+                netmask_edit,
+                urwid.Text(u"Status: " + ("up" if interface.is_up() else "down")),
+                urwid.Button(u"Apply", on_press=self.on_apply_if, user_data=(interface.name, address_edit, netmask_edit)),
             ]))
         return ifs_info_widget
 
-    def on_save(self):
-        return
+    def on_apply_if(self, button, if_info):
+        interface = ifconfig.findif(if_info[0])
+        with open('/etc/sysconfig/network-scripts/ifcfg-' + if_info[0], 'w') as out:
+            out.write('HWADDR={}\n'.format(interface.get_mac())
+                    + 'BOOTPROTO=static\n'
+                    + 'IPADDR={}\n'.format(if_info[1].get_edit_text())
+                    + 'PREFIX={}\n'.format(int(if_info[2].get_edit_text()))
+            )
+        os.system('/etc/init.d/network restart')
 
